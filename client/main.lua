4bd2e89703c1e148b54b4336205f081802490feb
Master Mind <48193931+iboss21@users.noreply.github.com>
2021-12-28 18:23:50 -0600

    updated client main.lua
    customized weight of trunks for advanced roleplay and to be realistic.

--- client/main.lua
+++ client/main.lua
@@ -555,43 +555,43 @@ RegisterCommand('inventory', function()
                     local maxweight = 0
                     local slots = 0
                     if vehicleClass == 0 then
-                        maxweight = 38000
-                        slots = 30
-                    elseif vehicleClass == 1 then
-                        maxweight = 50000
-                        slots = 40
-                    elseif vehicleClass == 2 then
-                        maxweight = 75000
-                        slots = 50
-                    elseif vehicleClass == 3 then
-                        maxweight = 42000
-                        slots = 35
-                    elseif vehicleClass == 4 then
-                        maxweight = 38000
-                        slots = 30
-                    elseif vehicleClass == 5 then
-                        maxweight = 30000
-                        slots = 25
-                    elseif vehicleClass == 6 then
-                        maxweight = 30000
-                        slots = 25
-                    elseif vehicleClass == 7 then
-                        maxweight = 30000
-                        slots = 25
-                    elseif vehicleClass == 8 then
                         maxweight = 15000
                         slots = 15
+                    elseif vehicleClass == 1 then
+                        maxweight = 20000
+                        slots = 20
+                    elseif vehicleClass == 2 then
+                        maxweight = 25000
+                        slots = 25
+                    elseif vehicleClass == 3 then
+                        maxweight = 17000
+                        slots = 15
+                    elseif vehicleClass == 4 then
+                        maxweight = 17000
+                        slots = 15
+                    elseif vehicleClass == 5 then
+                        maxweight = 13000
+                        slots = 10
+                    elseif vehicleClass == 6 then
+                        maxweight = 13000
+                        slots = 10
+                    elseif vehicleClass == 7 then
+                        maxweight = 13000
+                        slots = 10
+                    elseif vehicleClass == 8 then
+                        maxweight = 2500
+                        slots = 5
                     elseif vehicleClass == 9 then
-                        maxweight = 60000
-                        slots = 35
+                        maxweight = 65000
+                        slots = 40
                     elseif vehicleClass == 12 then
-                        maxweight = 120000
-                        slots = 35
+                        maxweight = 100000
+                        slots = 50
 		            elseif vehicleClass == 13 then
                         maxweight = 0
                         slots = 0
                     elseif vehicleClass == 14 then
-                        maxweight = 120000
+                        maxweight = 150000
                         slots = 50
                     elseif vehicleClass == 15 then
                         maxweight = 120000
